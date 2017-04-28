f84780716346a80493b4c6ab39da3c7bcdd9a71a
Nathan Clark <Nathan.Clark@nasa.gov>
2017-04-28 09:14:02 -0400

    Creating 0.9.1 snapshot release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject migratus "0.9.0"
+(defproject com.tokenshift/migratus "0.9.1-SNAPSHOT"
   :description "MIGRATE ALL THE THINGS!"
   :url "http://github.com/yogthos/migratus"
   :license {:name "Apache License, Version 2.0"
